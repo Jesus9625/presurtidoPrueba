4010e5d6cfd3e54174c1d4694de4c9afd25dce4d
Raquel Eunice Lopez Miranda <rlopezm@coppel.com>
2019-08-29 10:56:27 -0600

    Se crea proyecto Android

---
+++ app/src/main/java/com/cedisropa/pruebaarquitectura/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cedisropa.pruebaarquitectura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cedisropa.sdk.DatosGenerales;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
